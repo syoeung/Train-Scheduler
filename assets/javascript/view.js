9de2a380bc5220f9b6fce881c8625a7607a47492
Steven <yoeung@outlook.com>
2018-06-30 23:30:43 -0400

    final changes

---
+++ assets/javascript/view.js
@@ -0,0 +1,38 @@
+ // manipulating web page
+
+ // On Page Load
+$(document).ready(function(){
+
+	controller.captureFormFields();
+	model.initialDatabasePull();
+	setInterval(function() {model.initialDatabasePull()}, 60000);
+	view.updateCurrentTime();
+	setInterval(function() {view.updateCurrentTime()}, 1000);
+
+});
+
+// view object
+var view = {
+
+	// function to update the Train Schedule Table
+
+	updateTrainScheduleTable: () => {
+
+		controller.convertFrequency();
+
+		$('#train-schedule-body').append(
+			'<tr>'+
+				'<th scope="row">' + trainNumber + '</th>' +
+				'<td>' + trainLine + '</td>' +
+				'<td>' + trainDestination + '</td>' +
+				'<td>' + nextTrain + '</td>' +
+				'<td>' + minutesAway + '</td>' +
+				'<td>' + trainFrequency + '</td>' +
+				'<td>' + trainPlatform + '</td>' +
+			'</tr>'
+			);
+	},
+	updateCurrentTime: () => {
+		$('.currentTime').text(moment().format('h:mm:ss A'))
+	}
+};
